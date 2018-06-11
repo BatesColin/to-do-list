995ddd839c5fa1d1b93a775629f7dbf8642a6824
Colin Bates and Eddie Harris <colin@>
2018-06-11 14:37:55 -0700

    add to do list page with objects

--- js/scripts.js
+++ js/scripts.js
@@ -8,15 +8,42 @@ ToDoItem.prototype.all = function() {
   return "Do this: " + this.name + " By: " + this.doBy;
 }
 
+function resetFields() {
+    $("input#toDoList").val("");
+    $("input#doByDate").val("");
+
+}
+
+
 $(document).ready(function() {
   $("form#new-contact").submit(function(event) {
     event.preventDefault();
-    var inputItem = $("input#toDoList").val();
-    var inputDate = $("input#doByDate").val();
 
-    var toDo = new ToDoItem(inputItem, inputDate);
+  var toDo;
+    $(".taskInfo").each(function() {
+      var inputItem = $(this).find("input#toDoList").val();
+      var inputDate = $(this).find("input#doByDate").val();
 
-    //$("p.test").text("HELLO");
-    $("p.test1").text(toDo.all());
+      toDo = new ToDoItem(inputItem, inputDate);
+      //arr.push(toDo);
+    });
+
+
+
+  //  $("p.test").text();
+    $("ul#lists").append("<li><span class='list'>" + toDo.name + "</span></li>");
+
+
+$(".list").last().click(function() {
+
+  $(".output").show();
+  $(".do-output").text(toDo.name);
+  $(".date-output").text(toDo.doBy);
+
+ $("ul#addresses").text("");
+//   newContact.addresses.forEach(function(address) {
+//     $("ul#addresses").append("<li>" + address.fullAddress() + "</li>");
   });
+resetFields()
 });
+  });
